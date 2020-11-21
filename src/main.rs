049b381fcd0de0db2e112152913e7ad8375143c8
Antony Gordon <gordonad@umich.edu>
2020-11-21 13:54:21 -0500

    Start of Sprintz Code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod sprintz;
+use sprintz::SprintzCompressor;
+
 fn main() {
     println!("Hello, world!");
+    
+    let data: [f64;5] = [1.0, 2.0, 1.0, 3.0, 5.0];
+    
+    let sprz = SprintzCompressor::new(&data, 10);
 }
---
+++ src/sprintz.rs
@@ -0,0 +1,113 @@
+//mod sprintz;
+
+use std::io::Cursor;
+use std::io::Write;
+use std::vec::Vec;
+
+pub struct Forecaster {
+    previous: f64,
+    
+    
+}
+
+impl Forecaster {
+    
+    fn predict(&self, value: f64) -> f64 {
+        self.previous
+    }
+    
+    
+    fn error(&self, value: f64) -> f64 {
+        //Change later to bitwise XOR
+        value - self.predict(value)
+    }
+    
+    
+    //Trains the forecaster for a better prediction
+    //Out algorithm just returns the previous value
+    fn train(&mut self, value:  f64, error: f64) {
+        self.previous = value;
+    }
+}
+
+
+pub struct SprintzCompressor<'a>{
+    input: &'a[f64],
+    block: Vec<f64>,
+    
+    blockPos: usize,
+    blockSize : usize,
+    forecaster: Forecaster,
+    
+    
+}
+
+ impl SprintzCompressor<'_> {
+    
+    pub fn new<'a>(input: &'a[f64], blockSize: usize)-> SprintzCompressor{
+         SprintzCompressor {
+             input,
+             block: Vec::with_capacity(blockSize),//u32 into usize 
+             blockPos: 0, 
+             blockSize: blockSize,
+             forecaster: Forecaster{ previous: 0.0}
+         }
+         
+        
+    }
+    
+    fn compress<'a>(&mut self, output: &mut Cursor<Vec<u8>>) 
+
+    {
+        let len: u32 = output.get_ref().len() as u32;
+        
+        //Break down integer
+        let lenBytes: [u8; 4] = [
+            ((len >> 24) & 0xFF ) as u8, 
+             ((len >> 16) & 0xFF) as u8, 
+             ((len >> 8) & 0xFF) as u8, 
+             (len & 0xFF) as u8
+             ];
+
+        output.write(&lenBytes);
+        
+        for val in self.input {
+            
+           let compress = self.submitDouble(*val);
+           
+           if compress {
+            //Compress
+             } 
+        }
+        
+        //flush(); //Not implemented
+        
+        for error in self.block.iter() {
+            //Do stuff
+        
+        }
+        
+        output.flush();
+        
+    }
+    
+    
+    //submits a double to the block for compression
+    //Returns tureif the block is fill and should be send to the output stream
+    fn submitDouble(&mut self, value:f64 ) -> bool {
+     
+        if self.blockPos == self.blockSize {
+            self.blockPos = 0;
+            return true;
+        } else {
+        
+            let error = self.forecaster.error(value);
+            self.block[self.blockPos] = error;
+            self.blockPos += 1;  
+            self.forecaster.train(value, error);
+            
+           return false; 
+        }
+    }
+    
+}
